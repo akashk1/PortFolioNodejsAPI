e8dca322ae8381c5492052c12eb4c8929ca3b4d2
Akash Kumar Singh <akashkumarsingh3@gmail.com>
2021-04-05 23:55:42 +0530

    trade validator added for update trade

--- Routes/Trade/tradeValidator.js
+++ Routes/Trade/tradeValidator.js
@@ -8,9 +8,22 @@ exports.validate = (method) => {
         body("user_id", "user_id is required").exists(),
         body("trade_name", "trade_name is required").exists(),
         body("ticker_symbol", "ticker_symbol is required").exists(),
-        body("type", "type is reuired").isIn(["Buy", "Sell"]),
-        body("shares", "shares is required").isInt({ min: 1 }),
-        body("trade_price", "trade_price is required").isInt({ min: 1 }),
+        body("type", "type is reuired").isIn(["Buy", "Sell"]).exists(),
+        body("shares", "shares is required").isInt({ min: 1 }).exists(),
+        body("trade_price", "trade_price is required")
+          .isInt({ min: 1 })
+          .exists(),
+      ];
+    }
+    case "updateTrade": {
+      return [
+        body("ticker_symbol", "ticker_Symbol is required").exists(),
+        body("shares", "shares is required").isInt({ min: 0 }).exists(),
+        body("type", "type is reuired").isIn(["Buy", "Sell"]).exists(),
+        body("trade_price", "trade_price is required")
+          .isInt({ min: 1 })
+          .exists(),
+        body("trade_id", "trade_id is required").exists(),
       ];
     }
   }
